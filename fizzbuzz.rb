f3f18a96c39ae1441ee754e46f6d0a238df8c5ae
Julian George <jules104@gmail.com>
2019-08-17 18:03:30 +0100

    first commit fizzbuzz.rb

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+(1..20).each do |num|
+  if num % 5 == 0 && num % 3 == 0
+    puts  "#{num} --> FizzBuzz"
+  elsif num % 5 == 0
+    puts "#{num} --> Buzz"
+  elsif num % 3 == 0
+    puts "#{num} --> Fizz"
+  else
+    puts "#{num} --> #{num}"
+  end
+end
