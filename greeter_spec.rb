84bd54e865e54c7a35e75f27f667d0699983b9c2
Julian George <jules104@gmail.com>
2019-08-18 08:53:47 +0100

    Add greeter_spec.rb

---
+++ greeter_spec.rb
@@ -0,0 +1,13 @@
+require './greeter'
+
+describe 'Greeter' do
+  it 'greets Rico' do
+    expect(greet('Rico')).to eq 'Hello, Rico, how are you today?'
+  end
+end
+
+describe 'Greeter' do
+  it 'greets Chloë' do
+    expect(greet('Chloë')).to eq 'Hello, Chloë, how are you today?'
+  end
+end
